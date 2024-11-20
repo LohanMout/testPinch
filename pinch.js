73507a3a17f53835c14a910b5dc4653f04935c99
Moutquin <e2155871@cmaisonneuve.qc.ca>
2024-11-20 09:35:04 -0500

    vert

--- pinch.js
+++ pinch.js
@@ -5,3 +5,8 @@ window.addEventListener('wheel', function (e) {
      zoom -= e.deltaY / 100
      pseudoCanvas.style.transform = "scale(" + zoom + "%)"
 });
+
+window.addEventListener("touchmove", (e) => {
+    var touch = e.touches[0] || e.changedTouches[0];
+    pseudoCanvas.style.backgroundColor = "green";
+});
